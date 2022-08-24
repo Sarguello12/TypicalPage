4c909b7ea87cc1aa9245c0324124e8d455e81a5d
Samuel Arguello <80545434+Sarguello12@users.noreply.github.com>
2022-08-24 15:00:14 -0500

    fix: refactored components to be more reuseable

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
+import AuthContext from '../store/auth-context';
 
 const emailReducer = (state, action) => {
   if(action.type === "USER_INPUT"){
@@ -36,6 +38,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authctx = useContext(AuthContext);
 
   //object destructuring
   //this will allow useEffect only to run when the validity of the email or password has changed not just the value stored
@@ -74,42 +78,32 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input 
+          id="email" 
+          label="E-mail" 
+          type="email" 
+          isValid={emailIsValid} 
+          value={emailState.value} 
+          onChange={emailChangeHandler} 
+          onBlur={validateEmailHandler}
+        />
+        <Input 
+          id="password" 
+          label="Password" 
+          type="password" 
+          isValid={passwordIsValid} 
+          value={passwordState.value} 
+          onChange={passwordChangeHandler} 
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    //dummy logout function for better ide autocomplete
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storeUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    
+        if (storeUserLoggedInInfo === "1") {
+          setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem("isLoggedIn", "1");
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem("isLoggedIn", "1");
+        setIsLoggedIn(true);
+    };
+
+    return <AuthContext.Provider 
+                value={{
+                    isLoggedIn: isLoggedIn, 
+                    onLogout: logoutHandler, 
+                    onLogin: loginHandler,
+                }}
+            >
+                {props.children}
+            </AuthContext.Provider>;
+};
 
 export default AuthContext;
 
 //use props mostly to pass values as it allows you to make components reuseable ie button component
 //if you need to forward something through a lot of components or to a component that does something very specific consider using context
+
+//NOT FOR HIGH FREQUENCY CHANGES
+//Best for replacing long prop chains
